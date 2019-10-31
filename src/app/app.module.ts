2caebda2e0e15e33c3889c33c1fd2a9cfc2e47a4
Ho Duong <hxhduong4394@gmail.com>
2019-10-31 15:34:13 +0700

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {YoutubePlaylistComponent} from './youtube-playlist/youtube-playlist.component';
 import {YoutubePlayerComponent} from './youtube-player/youtube-player.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {YoutubePlayerComponent} from './youtube-player/youtube-player.component'
     YoutubePlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
